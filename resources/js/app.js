33485f759ddc5a1804178a4a1c39fa7f567cc8af
Louis-Alexandre Cazal <louis-alexandrecazal@OCPM-Admin-MBP.local>
2023-05-26 09:47:12 -0400

    Added Alpine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,8 @@ import './bootstrap';
 import '@fortawesome/fontawesome-free/js/brands';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/fontawesome';
+import Alpine from 'alpinejs'
+ 
+window.Alpine = Alpine
+ 
+Alpine.start()
